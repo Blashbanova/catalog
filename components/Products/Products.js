4738cdfbc49674e5161b9340adc610794129997b
Blashbanova <blashbanovaa@mail.ru>
2022-08-23 17:41:28 +0400

    Работа с кнопкой

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,19 +1,46 @@
 class Products {
+    constructor() {
+        this.classNameActive = 'products-element__btn_active';
+        this.labelAdd = 'Добавить в корзину';
+        this.labelRemove = 'Удалить из корзины';
+    }
+
+    handleSetLocationStorage(element, id) {
+        const { pushProduct, products } = localStorageUtil.putProducts(id);
+        
+        if (pushProduct) {
+            element.classList.add(this.classNameActive);
+            element.innerHTML = this.labelRemove;
+        } else {
+            element.classList.remove(this.classNameActive);
+            element.innerHTML = this.labelAdd;
+        }
+    }
 
     render() {
+        const productsStore = localStorageUtil.getProducts();
         let htmlCatalog = '';
 
-        CATALOG.forEach(({id, name, price, image}) => {
-            
+        CATALOG.forEach(({ id, name, price, image }) => {
+            let activeClass = '';
+            let activeText = '';
+
+            if (productsStore.indexOf(id) === -1) {
+                activeText = this.labelAdd;
+            } else {
+                activeClass = ' '+this.classNameActive;
+                activeText = this.labelRemove;
+            }
+
             htmlCatalog += `
-                <li class = "products-element">
-                    <span class="products-element__name"> ${name}</span>
+                <li class="products-element">
+                    <span class="products-element__name">${name}</span>
                     <img class="products-element__img" src="${image}" />
                     <span class="products-element__price">
                         ${price.toLocaleString()} ₽
                     </span>
-                    <button class="products-element__btn">
-                        Добавить в корзину
+                    <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">
+                        ${activeText}
                     </button>
                 </li>
             `;
